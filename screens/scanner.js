f24541cff349947df5045a7867f0cc97ea355073
obarosa <antonio.barosa@hotmail.com>
2022-05-17 18:09:04 +0100

    QR Code

--- screens/scanner.js
+++ screens/scanner.js
@@ -16,18 +16,74 @@ const Scanner = ({ navigation }) => {
 
     const handleBarCodeScanned = ({ type, data }) => {
         setScanned(true);
-        // alert(`${data} AND ${type}`);
-        var res = data.split(";");
-        var yau = [];
-        for (let i = 0; i < res.length; i++) {
-            var pontos = res[i].split('N:');
-            yau.push([pontos[i]]+pontos[i+1]);
+
+        // V - CARD (Versão 3.0)
+        var arr = data;
+        if (arr.includes('BEGIN:VCARD')) {
+            var username = arr.split('\n').slice(2, -1).find(d => d.includes('FN')).split(':')[1]; // Username
+            var priNome = username.split(' ')[0]; // First Name
+            var apelido = username.split(' ')[1]; // Last Name
+            var email = arr.split('\n').find(d => d.includes('EMAIL')).split(':')[1]; // Email
+
+            if (arr.split('\n').map(e => e.toLowerCase()).slice(2, -1).find(d => d.includes('tel'))) {
+                if (arr.split('\n').map(e => e.toLowerCase()).slice(2, -1).find(d => d.includes('cell'))) {
+                    var telemovel = arr.split('\n').map(e => e.toLowerCase()).slice(2, -1).find(d => d.includes('cell')).split(':')[1]
+                }
+                if (arr.split('\n').map(e => e.toLowerCase()).slice(2, -1).find(d => d.includes('home'))) {
+                    var telemovel = arr.split('\n').map(e => e.toLowerCase()).slice(2, -1).find(d => d.includes('home')).split(':')[1]
+                }
+                if (arr.split('\n').map(e => e.toLowerCase()).slice(2, -1).find(d => d.includes('work'))) {
+                    var escritorio = arr.split('\n').map(e => e.toLowerCase()).slice(2, -1).find(d => d.includes('work')).split(':')[1]
+                }
+            }
+
+            console.log(priNome);
+            console.log(apelido);
+            console.log(username);
+            console.log(email);
+            console.log(telemovel);
+            console.log(escritorio);
         }
-        console.log(yau);
-        console.log(typeof yau);
+
+        // MECARD
+        if (arr.includes('MECARD')) {
+            var mecard_username = arr.split(';').slice(0, -1).find(e => e.includes('N')).slice(7).split(':')[1]
+            var mecard_priNome = mecard_username.split(' ')[0]
+            var mecard_apelido = mecard_username.split(' ')[1]
+            var mecard_email = arr.split(';').map(d => d.toLowerCase()).slice(0, -1).find(e => e.includes('email')).split(':')[1]
+
+            let yau = arr.split(';')
+
+            let teles = [];
+            for (let i = 0; i < yau.length; i++) {
+                let element = yau[i].slice(4);
+                if (yau[i].includes('TEL')) {
+                    if (teles.length === 13) {
+                        teles.push(element.slice(4));
+                    } else {
+                        teles.push(element);
+                    }
+                }
+            }
+            console.log(teles);
+
+            var mecard_telemovel
+            var mecar_escritorio
+            for (let j = 0; j < teles.length; j++) {
+                if (teles[j].charAt(0) === '9') {
+                    mecard_telemovel = teles[j];
+                } else if (teles[j].charAt(0) === '2') {
+                    mecar_escritorio = teles[j];
+                }
+            }
+
+            console.log(mecar_escritorio);
+            console.log(mecard_email);
+        }
+
         Alert.alert(
             "Contacto Detectado!",
-            `${[res]}`,
+            `${[data]}`,
             [
                 {
                     text: "Cancel",
